0c58f30cfc54056846f701d183087f3e1f8ebac6
simolex <simolex@yandex.ru>
2022-02-22 19:05:33 +0300

    Задание №6

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,12 +1,11 @@
 const timer = (deadLine, hasDays = false) => {
   const timerID = document.getElementById("timer");
-  const timerHours = document.getElementById("timer-hours");
-  const timerMinutes = document.getElementById("timer-minutes");
-  const timerSeconds = document.getElementById("timer-seconds");
-  const timerMeridiem = document.getElementById("timer-meridiem");
+  const timerDays = document.getElementById("timer-days");
+  const timerRemaining = document.getElementById("timer-remaining");
 
   const dateStop = new Date(deadLine).getTime();
   let timerHandle;
+  const weeks = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
 
   const leadingZero = (num) => (num < 10 ? "0" + num : "" + num);
 
@@ -17,17 +16,22 @@ const timer = (deadLine, hasDays = false) => {
     let hours = Math.floor(timeRemaining / 3600);
     hours = hasDays ? hours % 24 : hours;
     const timerMeridiem = dateNow.toLocaleTimeString("en-US");
+    const weekName = weeks[dateNow.getDay()];
 
     return {
       timeRemaining,
       days: leadingZero(days),
       timerMeridiem,
+      weekName,
     };
   };
 
   const updateClock = () => {
     const getTime = getTimeRemaining();
+
     timerID.textContent = getTime.timerMeridiem;
+    timerDays.textContent = getTime.weekName;
+    timerRemaining.textContent = getTime.days;
   };
 
   timerHandle = setInterval(updateClock, 1000);
